4c10660b785bb9433d02f79149b685982bdacd28
Lisa Han <ellesis@gmail.com>
2019-09-09 19:35:21 -0700

    loading page - first page

--- App.js
+++ App.js
@@ -1,29 +1,8 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import Loading from './loading'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <View style={styles.yellowView}>
-        <Text>Hello!!! see ya!!</Text>
-      </View>
-      <View style={styles.blueView}>
-        <Text>Hello!!!!</Text>
-      </View>
-    </View>
+    <Loading />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1
-  },
-  yellowView: {
-    flex: 1,
-    backgroundColor: "yellow"
-  },
-  blueView: {
-    flex: 3, 
-    backgroundColor: "blue"
-  } 
-});
