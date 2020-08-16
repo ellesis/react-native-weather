4d8e938713e3efb05694191a597ed2c7d7d77b38
Lisa Han <ellesis@gmail.com>
2020-08-16 07:09:26 -0700

    update app

--- App.js
+++ App.js
@@ -1,7 +1,18 @@
 import React from 'react';
 import Loading from './loading'
+import * as Location from 'expo-location';
 
 export default function App() {
+  getLocation = async () => {
+    // need permition
+    const location = await Location.getCurrentPositionAsync(options)
+    console.log(location)
+  }
+
+  componentDidMount() {
+    this.getLocation()
+  }
+  
   return (
     <Loading />
   );
